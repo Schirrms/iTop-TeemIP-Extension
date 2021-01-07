0f337af2202a6508405310e9aa52b2aae8f511a4
Schirrms <schirrms@schirrms.net>
2021-01-07 18:16:13 +0100

    Add a virtualmachine_status field, some visual improvments for Enclosure (0.1.1.0)

--- schirrms-teemip-extension/module.schirrms-teemip-extension.php
+++ schirrms-teemip-extension/module.schirrms-teemip-extension.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'schirrms-teemip-extension/0.1.0',
+	'schirrms-teemip-extension/0.1.1',
 	array(
 		// Identification
 		//
