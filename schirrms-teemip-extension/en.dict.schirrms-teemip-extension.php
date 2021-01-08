c466bd242ac0b5d85a125a6cddbc82938163e26e
Schirrms <schirrms@schirrms.net>
2021-01-08 20:16:08 +0100

    Still displaying IP in genericCommint... (0.2.0.9)

--- schirrms-teemip-extension/en.dict.schirrms-teemip-extension.php
+++ schirrms-teemip-extension/en.dict.schirrms-teemip-extension.php
@@ -2,11 +2,29 @@
 /**
  * Localized data
  *
- * @copyright   Copyright (C) 2013 XXXXX
+ * @copyright   Copyright (C) 2011 Schirrms
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 
+//
+// Class: lnkGenericCommInterfaceToIPAddress
+//
+
 Dict::Add('EN US', 'English', 'English', array(
-	// Dictionary entries go here
+	'Class:lnkGenericCommInterfaceToIPAddress' => 'Link Comm Interface/ IP Address',
+	'Class:lnkGenericCommInterfaceToIPAddress+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id_finalclass_recall' => 'Comm Type',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id_finalclass_recall+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_id' => 'Comm Interface',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_name' => 'Comm Interface Name',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_name+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id' => 'IP Address',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:usage_id' => 'IP Address usage',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:usage_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_org_name' => 'IP Address organization name',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_org_name+' => '',
+
 ));
 ?>
---
+++ schirrms-teemip-extension/fr.dict.schirrms-teemip-extension.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2021 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: lnkGenericCommInterfaceToIPAddress
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:lnkGenericCommInterfaceToIPAddress' => 'Lien Interface de Communication / Adresse IP',
+	'Class:lnkGenericCommInterfaceToIPAddress+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id_finalclass_recall' => 'Type d\'IP',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id_finalclass_recall+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_id' => 'Interface de Communication',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_name' => 'Nom Interface de Comm',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipinterface_name+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id' => 'Adresse IP',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:usage_id' => 'Utilisation de l\'adresse IP',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:usage_id+' => '',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_org_name' => 'Nom de l\'organisation de l\'adresse IP',
+	'Class:lnkGenericCommInterfaceToIPAddress/Attribute:ipaddress_org_name+' => '',
+));
+
+//
+// Class: GenericCommVirtInterface
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+        'Class:GenericCommVirtInterface/Attribute:vlans_list' => 'VLANs',
+        'Class:GenericCommVirtInterface/Attribute:vlans_list+' => '',
+        'Class:GenericCommVirtInterface/Attribute:vrfs_list' => 'VRFs',
+        'Class:GenericCommVirtInterface/Attribute:vrfs_list+' => '',
+        'Class:GenericCommVirtInterface/Attribute:status' => 'Statut',
+        'Class:GenericCommVirtInterface/Attribute:status+' => '',
+        'Class:GenericCommVirtInterface/Attribute:status/Value:active' => 'Active',
+        'Class:GenericCommVirtInterface/Attribute:status/Value:active+' => '',
+        'Class:GenericCommVirtInterface/Attribute:status/Value:inactive' => 'Inactive',
+        'Class:GenericCommVirtInterface/Attribute:status/Value:inactive+' => '',
+));
+
+//
+// Class: lnkGenericCommInterfaceToVLAN
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+        'Class:lnkGenericCommInterfaceToVLAN' => 'Lien Interface réseau / VLAN',
+        'Class:lnkGenericCommInterfaceToVLAN+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_id' => 'Interface de comm',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_id+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_name' => 'Nom interface de comm',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_name+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_device_id' => 'Equipement',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_device_id+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_device_name' => 'Nom équipement',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:GenericCommInterface_device_name+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:vlan_id' => 'VLAN',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:vlan_id+' => '',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:vlan_tag' => 'VLAN Tag',
+        'Class:lnkGenericCommInterfaceToVLAN/Attribute:vlan_tag+' => '',
+));
+
+?>
